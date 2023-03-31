a8469d9a08f070776923d29a5a234738881fd011
mohamed moussa <mohamedmousa2552001@gmail>
2023-03-30 18:12:07 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *_strcmp - this function to compy string
+ *@s1: frist pointer
+ *@s2: seconed pointer
+ *Return: integer value
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int flag = 0;
+
+	while (*s1 != '\0')
+	{
+		if ( *s1 > *s2 )
+			flag = s1 - s2;
+		if ( *s1 < *s2 )
+			flag = s1 - s2;
+		s1++;
+		s2++;
+	}
+	return flag;
+}
+	
+
