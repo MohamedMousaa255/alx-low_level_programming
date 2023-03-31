62262fa549024453fe5b13d30d534a10cd5b4ba6
mohamed moussa <mohamedmousa2552001@gmail>
2023-03-30 19:10:55 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int temp;
+
+	while ((n - 1) != i)
+	{
+		temp = *(a + i);
+		*(a + i) = *(a + n);
+		*(a + n) = temp;
+		i++;
+		n--;
+	}
+}
+
+
+
