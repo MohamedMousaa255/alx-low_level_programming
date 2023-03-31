6c72a0c2323bf3c4da7a31e61272e1404318d64d
mohamed moussa <mohamedmousa2552001@gmail>
2023-03-30 18:36:11 -0700

    task ~

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,16 +10,18 @@ int _strcmp(char *s1, char *s2)
 {
 	int flag = 0;
 
-	while (*s1 != '\0')
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
 	{
-		if ( *s1 > *s2 )
-			flag = s1 - s2;
-		if ( *s1 < *s2 )
-			flag = s1 - s2;
-		s1++;
-		s2++;
+		 s1++;
+		 s2++;
 	}
-	return flag;
+	if (*s1 == *s2)
+	{
+		return (flag);
+	}
+	if (*s1 != *s2)
+	{
+		flag = *s1 - *s2;
+	}
+	return (flag);
 }
-	
-
