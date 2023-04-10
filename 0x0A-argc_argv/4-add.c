22e7f6c004faaeba5b442999a5d2fd5a868be65c
mohamed moussa <mohamedmousa2552001@gmail>
2023-04-09 18:42:19 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+/**
+ *main - main function
+ *@argc: prameteri for argument
+ *@argv: pointer to array of argument array
+ *Return: interger zero
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1;
+	int flag = 1;
+	int j = 1;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (-1);
+	}
+	while (i < argc)
+	{
+		if (!(isdigit(*argv[i])))
+		{
+			printf("Error\n");
+			return (1);
+			flag = 0;
+		}
+		i++;
+	}
+	if (flag == 1)
+	{
+		while (j < argc)
+		{
+			sum += atoi(argv[j]);
+			j++;
+		}
+		printf("%d\n", sum);
+	}
+
+
+	return (0);
+}
