8ec9dc51fc0b3955d8f53535460a8553334151ee
mohamed moussa <mohamedmousa2552001@gmail>
2023-04-10 06:43:13 -0700

    5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -24,30 +24,33 @@ int main(int argc, char *argv[])
 	{	N = atoi(argv[1]);
 		if (N < 0)
 			printf("0\n");
-		while (R != 0)
+		else 
 		{
-			if (i == 0)
-			{	n = n + (N / 25);
-				R = (N % 25), N = R;
-				i++; }
-			if (i == 1)
-			{	n = n + (N / 10);
-				R = (N % 10), N = R;
-				i++; }
-			if (i == 2)
-			{	n = n + (N / 5);
-				R = (N % 5), N = R;
-				i++; }
-			if (i == 3)
-			{	n = n + (N / 2);
-				R = (N % 2), N = R;
-				i++; }
-			if (i == 4)
-			{	n = n + (N / 1);
-				R = (N % 1), N = R;
-				i++; }
+			while (R != 0)
+			{
+				if (i == 0)
+				{	n = n + (N / 25);
+					R = (N % 25), N = R;
+					i++; }
+				if (i == 1)
+				{	n = n + (N / 10);
+					R = (N % 10), N = R;
+					i++; }
+				if (i == 2)
+				{	n = n + (N / 5);
+					R = (N % 5), N = R;
+					i++; }
+				if (i == 3)
+				{	n = n + (N / 2);
+					R = (N % 2), N = R;
+					i++; }
+				if (i == 4)
+				{	n = n + (N / 1);
+					R = (N % 1), N = R;
+					i++; }
+			}
+			 printf("%d\n", n);
 		}
-		printf("%d\n", n);
 	}
 	return (0);
 }
