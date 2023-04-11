c440f8c51c9bb23a06cb283be462e0c8bb481163
mohamed moussa <mohamedmousa2552001@gmail>
2023-04-11 16:15:49 -0700

    1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ *_strdup - function to alloc new mem
+ *@str: pointer of string
+ *Return: char pointer
+ */
+char *_strdup(char *str)
+{
+	int i = 1;
+	int j = 0;
+	char *ptr;
+
+	if (str == 0)
+		return (NULL);
+	while (str[i] != '\0')
+		i++;
+	ptr = (char *)malloc(i);
+	while (*str != '\0')
+	{
+		ptr[j] = *str;
+		str++;
+		j++;
+	}
+	j++;
+	ptr[j] = '\0';
+
+	return (ptr);
+
+}
