2b97ad8f632a83a7ca773663fb711f2ba118b29b
mohamed moussa <mohamedmousa2552001@gmail>
2023-04-10 18:34:38 -0700

    0

--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,4 +1,16 @@
 #include "main.h"
-char *_strncpy(char *dest, char *src, int n)
+/**
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: char to check
+ * @src: char to check
+ * Return: 0 is success
+ */
+char *_strcpy(char *dest, char *src)
 {
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
 }
