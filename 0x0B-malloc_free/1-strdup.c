6a023697c026752586900f8c7bd45ee6fdf9a20f
mohamed moussa <mohamedmousa2552001@gmail>
2023-04-11 16:28:48 -0700

    1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,7 @@
  */
 char *_strdup(char *str)
 {
-	int i = 1;
+	int i = 0;
 	int j = 0;
 	char *ptr;
 
@@ -17,16 +17,21 @@ char *_strdup(char *str)
 		return (NULL);
 	while (str[i] != '\0')
 		i++;
-	ptr = (char *)malloc(i+1);
-	while (*str != '\0')
+	ptr = (char *)malloc(i * sizeof(*str) + 1);
+	if (m == 0)
+		return (NULL);
+	else if (m != 0)
 	{
-		ptr[j] = *str;
-		str++;
-		j++;
-	}
-	j++;
-	ptr[j] = '\0';
 
+		while (*str != '\0')
+		{
+			ptr[j] = *str;
+			str++;
+			j++;
+		}
+		j++;
+		ptr[j] = '\0';
+	}
 	return (ptr);
 
 }
