281e3be15ce62d247b7bf7d363820113dcf0eb69
mohamed moussa <mohamedmousa2552001@gmail>
2023-04-11 17:18:44 -0700

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,10 +15,16 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	char *ptr;
 
-	while (s1[size1] != '\0')
+	if (s1 != NULL)
+	{
+		while (s1[size1] != '\0')
 		size1++;
-	while (s2[size2] != '\0')
+	}
+	if (s2 != NULL)
+	{
+		while (s2[size2] != '\0')
 		size2++;
+	}
 	if (s1 == NULL)
 		*s1 = '\0';
 	if (s2 == NULL)
